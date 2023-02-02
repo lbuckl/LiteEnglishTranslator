43b9183cc3c40ef963d8bb5363bd05e3f9cb1e92
lbuckl <ya_mvn@mail.ru>
2023-02-02 19:34:31 +0500

    Часть 1: Данные выводятся, получается ответ от API но перевода пока не видно на экране

--- app/src/main/java/com/vados/liteenglishtranslator/ui/MainActivity.kt
+++ app/src/main/java/com/vados/liteenglishtranslator/ui/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.vados.liteenglishtranslator.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.vados.liteenglishtranslator.R
 import com.vados.liteenglishtranslator.databinding.ActivityMainBinding
 import com.vados.liteenglishtranslator.domain.AppState
+import com.vados.liteenglishtranslator.domain.DataModel
 import com.vados.liteenglishtranslator.presenter.MainPresenter
 import com.vados.liteenglishtranslator.presenter.Presenter
 import com.vados.liteenglishtranslator.ui.base.BaseActivity
@@ -15,12 +20,52 @@ class MainActivity: BaseActivity<AppState>() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var adapter: MainRVAdapter? = null
+
     override fun createPresenter(): Presenter<AppState, BaseView> {
         return MainPresenter()
     }
 
+    private val onListItemClickListener: MainRVAdapter.OnListItemClickListener =
+        object : MainRVAdapter.OnListItemClickListener {
+            override fun onItemClick(data: DataModel) {
+                Toast.makeText(this@MainActivity, data.text, Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun renderData(appState: AppState) {
-        //TODO(Необходима реализация вывода данных на кэран)
+        when (appState) {
+            is AppState.Success -> {
+                val dataModel = appState.data
+                if (dataModel == null || dataModel.isEmpty()) {
+                    showErrorScreen(getString(R.string.empty_server_response_on_success))
+                } else {
+                    showViewSuccess()
+                    if (adapter == null) {
+                        binding.mainActivityRecyclerview.layoutManager =
+                            LinearLayoutManager(applicationContext)
+                        binding.mainActivityRecyclerview.adapter =
+                            MainRVAdapter(onListItemClickListener, dataModel)
+                    } else {
+                        adapter!!.setData(dataModel)
+                    }
+                }
+            }
+            is AppState.Loading -> {
+                showViewLoading()
+                if (appState.progress != null) {
+                    binding.progressBarHorizontal.visibility = View.VISIBLE
+                    binding.progressBarRound.visibility = View.GONE
+                    binding.progressBarHorizontal.progress = appState.progress
+                } else {
+                    binding.progressBarHorizontal.visibility = View.GONE
+                    binding.progressBarRound.visibility = View.VISIBLE
+                }
+            }
+            is AppState.Error -> {
+                showErrorScreen(appState.error.message)
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +73,51 @@ class MainActivity: BaseActivity<AppState>() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initFabClickListener()
+    }
+
+    private fun initFabClickListener(){
+        binding.searchFab.setOnClickListener {
+            val searchDialogFragment = SearchDialogFragment.newInstance()
+            searchDialogFragment.setOnSearchClickListener(object :
+                SearchDialogFragment.OnSearchClickListener {
+                override fun onClick(searchWord: String) {
+                    presenter.getData(searchWord, true)
+                }
+            })
+            searchDialogFragment.show(supportFragmentManager, BOTTOM_SHEET_FRAGMENT_DIALOG_TAG)
+        }
+    }
+
+    private fun showErrorScreen(error: String?) {
+        showViewError()
+        binding.errorTextview.text = error ?: getString(R.string.undefined_error)
+        binding.reloadButton.setOnClickListener {
+            presenter.getData("hi", true)
+        }
+    }
+
+    private fun showViewSuccess() {
+        binding.successLinearLayout.visibility = View.VISIBLE
+        binding.loadingFrameLayout.visibility = View.GONE
+        binding.errorLinearLayout.visibility = View.GONE
+    }
+
+    private fun showViewLoading() {
+        binding.successLinearLayout.visibility = View.GONE
+        binding.loadingFrameLayout.visibility = View.VISIBLE
+        binding.errorLinearLayout.visibility = View.GONE
+    }
+
+    private fun showViewError() {
+        binding.successLinearLayout.visibility = View.GONE
+        binding.loadingFrameLayout.visibility = View.GONE
+        binding.errorLinearLayout.visibility = View.VISIBLE
+    }
+
+    companion object {
+        private const val BOTTOM_SHEET_FRAGMENT_DIALOG_TAG =
+            "74a54328-5d62-46bf-ab6b-cbf5fgt0-092395"
     }
 }
